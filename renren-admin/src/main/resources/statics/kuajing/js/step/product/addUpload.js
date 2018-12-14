3b7f100517b7bc409a616ad553bb35295f366692
1722655598@qq.com <Linlin521>
2018-12-14 19:54:03 +0800

    上传产品定时上传，亚马逊一级子级分类，立即上传

--- renren-admin/src/main/resources/statics/kuajing/js/step/product/addUpload.js
+++ renren-admin/src/main/resources/statics/kuajing/js/step/product/addUpload.js
@@ -8,29 +8,7 @@ window.onload = function () {
 
 
 }
-var html1 = $('<div class="qita"></div>');
-var html2 = $('<div class="some-content-related-div" style="width: 100%;margin: 0 auto;"></div>');
-var html3 = $('<div class="inner-content-div2"></div>');
-var ul = $('<ul></ul>');
-//分类item
-var fenlItem = Vue.extend({
-    name: 'fenl-item',
-    props:{item:{}},
-    template:[
-        '<div class="qita">',
-        '<div class="some-content-related-div" style="width: 100%;margin: 0 auto;">',
-        '<div class="inner-content-div2">',
-        '<ul>',
-        '<li v-for="item in leven1" :id="item.id">{{ item.value }}</li>',
-        '</ul>',
-        '</div>',
-        '</div>',
-        '</div>',
-    ].join('')
 
-});
-//注册菜单组件
-Vue.component('fenlItem',fenlItem);
 
 
 
@@ -40,14 +18,16 @@ var vm = new Vue({
         value9:null,
         startId: null,
         endId: null,
+        uploadIdsstr:'',
         uploadIds:[],
         grantShopId: 0,
         isAttribute: 0,
         grantShop:null,
         amazonCategoryId: 0,
         amazonCategory: null,
-        amazonTemplateId: 0,
-        amazonTemplate: null,
+        amazonTemplateId: 10,
+        amazonTemplate: 'test模板',
+        inputche:[],
         operateItem: [],
         marketplace:[],
         shopinfo:{
@@ -80,7 +60,7 @@ var vm = new Vue({
                 shadeClose: true,
                 btn: ['确定','取消'],
                 btn1: function (index) {
-
+                    layer.close(index);
 
                 },
                 btn2: function (index) {
@@ -92,47 +72,120 @@ var vm = new Vue({
 
         // 定时上传
         timeUpFunc:function () {
-            layer.open({
-                type: 1,
-                title: false,
-                content: $('#timeUp'), //这里content是一个普通的String
-                skin: 'openClass',
-                area: ['530px', '330px'],
-                shadeClose: true,
-                btn: ['上传','取消'],
-                btn1: function (index) {
+            if (this.shopinfo.region!=undefined) {
+                layer.open({
+                    type: 1,
+                    title: false,
+                    content: $('#timeUp'), //这里content是一个普通的String
+                    skin: 'openClass',
+                    area: ['530px', '330px'],
+                    shadeClose: true,
+                    btn: ['上传', '取消'],
+                    btn1: function (index) {
+                        console.log(vm.inputche)
+                        vm.uploadIds = vm.uploadIdsstr.split(',');
+                        console.log(vm.uploadIds);
+                        if (vm.inputche[0]==true){
 
-                },
-                btn2: function (index) {
+                            vm.operateItem = [0,1,2,3,4];
+                        }else {
+                            for (var i=0;i<vm.inputche.length;i++){
+                                if (vm.inputche[i]==true){
+                                    console.log('2222');
+                                    vm.operateItem.push(i-1);
+                                }
+                            }
+                        }
+                        console.log(vm.operateItem);
+                        vm.grantShopId = vm.shopinfo.grantShopId;
+                        vm.grantShop = vm.shopinfo.shopName;
+                        console.log(vm);
+                        $.ajax({
+                            url: '../../product/upload/addUpload',
+                            type: 'post',
+                            data: {
+                                'startId': vm.startId,
+                                'endId': vm.endId,
+                                'uploadIds': vm.uploadIds,
+                                'grantShopId': vm.grantShopId,
+                                'isAttribute': vm.isAttribute,
+                                'grantShop':vm.grantShop,
+                                'amazonCategoryId': vm.amazonCategoryId,
+                                'amazonCategory': vm.amazonCategory,
+                                'amazonTemplateId': vm.amazonTemplateId,
+                                'amazonTemplate': vm.amazonTemplate,
+                                'operateItem': vm.operateItem,
+                                'time':vm.changeTime,
+                                'countryCode':vm.shopinfo.countryCode,
+                            },
+                            dataType: 'json',
+                            success: function (r) {
+                                console.log(r);
+                                if (r.code === 0) {
+                                    layer.close(index)
+                                } else {
+                                    layer.alert(r.message);
+                                }
 
 
-                }
-            });
+                            },
+                            error: function () {
+                                layer.msg("网络故障");
+                            }
+                        });
+                    },
+                    btn2: function (index) {
+
+
+                    }
+                });
+            }else {
+                layer.msg("请选择店铺");
+            }
         },
         //立即上传
         addUpload:function () {
+            console.log(vm.inputche)
+            vm.uploadIds = vm.uploadIdsstr.split(',');
+            console.log(vm.uploadIds);
+            if (vm.inputche[0]==true){
+
+                vm.operateItem = [0,1,2,3,4];
+            }else {
+                for (var i=0;i<vm.inputche.length;i++){
+                    if (vm.inputche[i]==true){
+                        console.log('2222');
+                        vm.operateItem.push(i-1);
+                    }
+                }
+            }
+            console.log(vm.operateItem);
+            vm.grantShopId = vm.shopinfo.grantShopId;
+            vm.grantShop = vm.shopinfo.shopName;
+            console.log(vm);
             $.ajax({
                 url: '../../product/upload/addUpload',
                 type: 'post',
-                data: {
-                    'startId': this.startId,
-                    'endId': this.endId,
-                    'uploadIds': this.uploadIds,
-                    'grantShopId': this.grantShopId,
-                    'isAttribute': this.isAttribute,
-                    'grantShop':this.grantShop,
-                    'amazonCategoryId': this.amazonCategoryId,
-                    'amazonCategory': this.amazonCategory,
-                    'amazonTemplateId': this.amazonTemplateId,
-                    'amazonTemplate': this.amazonTemplate,
-                    'operateItem': this.operateItem,
-                },
-                dataType: 'json',
+                data: JSON.stringify({
+                    'startId': parseInt(vm.startId),
+                    'endId': parseInt(vm.endId),
+                    'uploadIds': vm.uploadIds,
+                    'grantShopId': parseInt(vm.grantShopId),
+                    'isAttribute': vm.isAttribute,
+                    'grantShop':vm.grantShop,
+                    'amazonCategoryId': vm.amazonCategoryId,
+                    'amazonCategory': vm.amazonCategory,
+                    'amazonTemplateId': vm.amazonTemplateId,
+                    'amazonTemplate': vm.amazonTemplate,
+                    'operateItem': vm.operateItem,
+                }),
+                contentType: "application/json",
+                // dataType: 'json',
                 success: function (r) {
                     console.log(r);
                     if (r.code === 0) {
                     } else {
-                        layer.alert(r.message);
+                        layer.alert(r.msg);
                     }
 
 
@@ -144,6 +197,7 @@ var vm = new Vue({
         },
         //授权店铺
         getmarketplace:function () {
+
             $.ajax({
                 url: '../../amazon/amazongrantshop/myShopList',
                 type: 'get',
@@ -196,24 +250,62 @@ var vm = new Vue({
 
         },
         // 子级分类
-        amazonItemCategory:function (event) {
+        amazonItemCategory:function (list) {
             var _index = $(event.target).attr('data-index');
             var index = parseInt(_index) + 1;
             vm.leven.splice(index);
+            console.log(list);
+            if (list.ifNext=='true') {
+                $.ajax({
+                    url: '../../product/amazoncategory/childCategoryList',
+                    type: 'post',
+                    data: {
+                        amazonCategoryId:list.amazonCategoryId
+                    },
+                    dataType: 'json',
+                    success: function (r) {
+                        console.log(r);
+                        if (r.code === 0) {
+                            vm.leven.push(r.amazonCategoryEntityChildList);
+                        } else {
+                            layer.alert(r.message);
+                        }
+                    },
+                    error: function () {
+                        layer.msg("网络故障");
+                    }
+                });
+            }else {
+                vm.amazonCategoryId = list.amazonCategoryId;
+                vm.amazonCategory = list.displayName;
+            }
 
-            vm.leven.push([{
-                'amazonCategoryId':0,
-                'ifNext':true,
-                'displayName':'分类1111'
-            },{
-                'amazonCategoryId':0,
-                'ifNext':true,
-                'displayName':'分类222'
-            },{
-                'amazonCategoryId':0,
-                'ifNext':true,
-                'displayName':'分类333'
-            }])
+        },
+        //时区转换
+        timeZoneConversion:function () {
+            console.log(vm.value9);
+            console.log(vm.shopinfo.countryCode);
+            $.ajax({
+                url: '../../product/upload/timeZoneConversion',
+                type: 'get',
+                data: {
+                    countryCode:vm.value9,
+                    countryTime:vm.shopinfo.countryCode
+                },
+                // contentType: "application/json",
+                dataType: 'json',
+                success: function (r) {
+                    console.log(r);
+                    if (r.code === 0) {
+                        vm.changeTime = r.CNTimeString;
+                    } else {
+                        layer.alert(r.msg);
+                    }
+                },
+                error: function () {
+                    layer.msg("网络故障");
+                }
+            });
         }
     },
     created:function () {
