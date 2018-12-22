d099460f6cbc6114c7041f882c3bc77150dce41f
594340717 <594340717@qq.com>
2018-12-22 11:13:04 +0800

    公告通知

---
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/NoticeController.java
@@ -0,0 +1,140 @@
+package io.renren.modules.sys.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import io.renren.common.validator.ValidatorUtils;
+import io.renren.modules.sys.entity.SysUserEntity;
+import io.renren.modules.sys.service.SysUserService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.renren.modules.sys.entity.NoticeEntity;
+import io.renren.modules.sys.service.NoticeService;
+import io.renren.common.utils.PageUtils;
+import io.renren.common.utils.R;
+
+
+
+/**
+ * 消息表
+ *
+ * @author wdh
+ * @email 594340717@qq.com
+ * @date 2018-12-22 03:22:35
+ */
+@RestController
+@RequestMapping("sys/notice")
+public class NoticeController extends AbstractController{
+    @Autowired
+    private NoticeService noticeService;
+    @Autowired
+    private SysUserService userService;
+    /**
+     * 查询未读消息数量
+     */
+    @RequestMapping("/count")
+    public R count(){
+        int count = noticeService.selectCount(new EntityWrapper<NoticeEntity>().eq("user_id",getUserId()).eq("notice_state",0));
+        return R.ok().put("count",count);
+    }
+    /**
+     * 标记为已读
+     */
+    @RequestMapping("/sign")
+    public R sign(@RequestParam Long noticeId){
+        NoticeEntity notice = noticeService.selectById(noticeId);
+        notice.setNoticeState(1);
+        noticeService.updateById(notice);
+        return R.ok();
+    }
+    /**
+     * 全部标记为已读
+     */
+    @RequestMapping("/signAll")
+    public R signAll(){
+        List<NoticeEntity> noticeList = noticeService.selectList(new EntityWrapper<NoticeEntity>().eq("user_id",getUserId()).eq("notice_state",0));
+        for(NoticeEntity notice : noticeList){
+            notice.setNoticeState(1);
+        }
+        noticeService.updateBatchById(noticeList);
+        return R.ok();
+    }
+    /**
+     * 全部列表
+     */
+    @RequestMapping("/list")
+    public R list(@RequestParam Map<String, Object> params){
+        PageUtils page = noticeService.queryPage(params);
+        return R.ok().put("page", page);
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    public R delete(@RequestBody Long[] noticeIds){
+        noticeService.deleteBatchIds(Arrays.asList(noticeIds));
+        return R.ok();
+    }
+
+    /**
+     * 发布通知
+     */
+    public R deliverNotice(@RequestBody NoticeEntity notice){
+        List<SysUserEntity> userList = userService.selectList(new EntityWrapper<SysUserEntity>());
+        List<NoticeEntity> noticeList = new ArrayList<NoticeEntity>();
+        for(SysUserEntity user : userList){
+            NoticeEntity no = new NoticeEntity();
+            no.setNoticeType("公告通知");
+            no.setUserId(user.getUserId());
+            no.setNoticeContent(notice.getNoticeContent());
+            noticeList.add(no);
+        }
+        noticeService.insertBatch(noticeList);
+        return R.ok();
+    }
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{noticeId}")
+    @RequiresPermissions("sys:notice:info")
+    public R info(@PathVariable("noticeId") Long noticeId){
+        NoticeEntity notice = noticeService.selectById(noticeId);
+
+        return R.ok().put("notice", notice);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    @RequiresPermissions("sys:notice:save")
+    public R save(@RequestBody NoticeEntity notice){
+        noticeService.insert(notice);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    @RequiresPermissions("sys:notice:update")
+    public R update(@RequestBody NoticeEntity notice){
+        ValidatorUtils.validateEntity(notice);
+        noticeService.updateAllColumnById(notice);//全部更新
+        return R.ok();
+    }
+
+
+}
---
+++ renren-admin/src/main/java/io/renren/modules/sys/service/NoticeService.java
@@ -0,0 +1,20 @@
+package io.renren.modules.sys.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import io.renren.common.utils.PageUtils;
+import io.renren.modules.sys.entity.NoticeEntity;
+
+import java.util.Map;
+
+/**
+ * 消息表
+ *
+ * @author wdh
+ * @email 594340717@qq.com
+ * @date 2018-12-22 03:22:35
+ */
+public interface NoticeService extends IService<NoticeEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+}
+
