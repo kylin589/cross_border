af163c7f7b351cf7fa0f738d4edc593a7373f0e1
zjr <1981763981@qq.com>
2019-01-07 14:52:34 +0800

    添加阿里云api依赖

--- renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
@@ -27,11 +27,9 @@ import io.renren.modules.product.entity.OrderEntity;
 import io.renren.modules.product.service.EanUpcService;
 import io.renren.modules.product.service.OrderService;
 import io.renren.modules.sys.entity.*;
-import io.renren.modules.sys.service.NoticeService;
-import io.renren.modules.sys.service.SysDeptService;
-import io.renren.modules.sys.service.SysRoleService;
-import io.renren.modules.sys.service.SysUserRoleService;
+import io.renren.modules.sys.service.*;
 import io.renren.modules.sys.shiro.ShiroUtils;
+import org.apache.commons.lang.RandomStringUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,30 +48,32 @@ import java.util.Date;
 
 /**
  * 登录相关
- * 
+ *
  * @author chenshun
  * @email sunlightcs@gmail.com
  * @date 2016年11月10日 下午1:15:31
  */
 @Controller
-public class SysLoginController extends AbstractController{
-	@Autowired
-	private Producer producer;
-	@Autowired
-	private SysDeptService deptService;
-	@Autowired
-	private OrderService orderService;
-	@Autowired
-	private EanUpcService eanUpcService;
-	@Autowired
-	private NoticeService noticeService;
-	@Autowired
-	private SysUserRoleService sysUserRoleService;
-	@Autowired
-	private SysRoleService roleService;
+public class SysLoginController extends AbstractController {
+    @Autowired
+    private Producer producer;
+    @Autowired
+    private SysDeptService deptService;
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private EanUpcService eanUpcService;
+    @Autowired
+    private NoticeService noticeService;
+    @Autowired
+    private SysUserRoleService sysUserRoleService;
+    @Autowired
+    private SysRoleService roleService;
+    @Autowired
+    private SysUserService sysUserService;
 
-	@RequestMapping("captcha.jpg")
-	public void captcha(HttpServletResponse response)throws IOException {
+    @RequestMapping("captcha.jpg")
+    public void captcha(HttpServletResponse response) throws IOException {
         response.setHeader("Cache-Control", "no-store, no-cache");
         response.setContentType("image/jpeg");
 
@@ -83,101 +83,134 @@ public class SysLoginController extends AbstractController{
         BufferedImage image = producer.createImage(text);
         //保存到shiro session
         ShiroUtils.setSessionAttribute(Constants.KAPTCHA_SESSION_KEY, text);
-        
+
         ServletOutputStream out = response.getOutputStream();
         ImageIO.write(image, "jpg", out);
-	}
-	
-	/**
-	 * 登录
-	 */
-	@ResponseBody
-	@RequestMapping(value = "/sys/login", method = RequestMethod.POST)
-	public R login(String username, String password, String captcha) {
-		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
-		if(!captcha.equalsIgnoreCase(kaptcha)){
-			return R.error("验证码不正确");
-		}
-		
-		try{
-			Subject subject = ShiroUtils.getSubject();
-			UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-			subject.login(token);
-		}catch (UnknownAccountException e) {
-			return R.error(e.getMessage());
-		}catch (IncorrectCredentialsException e) {
-			return R.error("账号或密码不正确");
-		}catch (LockedAccountException e) {
-			return R.error("账号已被锁定,请联系管理员");
-		}catch (AuthenticationException e) {
-			return R.error("账户验证失败");
-		}
+    }
 
-		new CalculationThread().start();
-		return R.ok();
-	}
+    /**
+     * 登录
+     */
+    @ResponseBody
+    @RequestMapping(value = "/sys/login", method = RequestMethod.POST)
+    public R login(String username, String password, String captcha) {
+        String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
+        if (!captcha.equalsIgnoreCase(kaptcha)) {
+            return R.error("验证码不正确");
+        }
 
-	class CalculationThread extends Thread   {
-		@Override
-		public void run() {
+        try {
+            Subject subject = ShiroUtils.getSubject();
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            subject.login(token);
+        } catch (UnknownAccountException e) {
+            return R.error(e.getMessage());
+        } catch (IncorrectCredentialsException e) {
+            return R.error("账号或密码不正确");
+        } catch (LockedAccountException e) {
+            return R.error("账号已被锁定,请联系管理员");
+        } catch (AuthenticationException e) {
+            return R.error("账户验证失败");
+        }
 
-			//总部查询MEN码
-			if(getUserId() == 1L){
-				int count = eanUpcService.selectCount(new EntityWrapper<EanUpcEntity>().eq("state",0));
-				if(count<1000){
-					NoticeEntity notice = new NoticeEntity();
-					notice.setDeptId(getDeptId());
-					notice.setUserId(getUserId());
-					notice.setNoticeContent("UPC/EAN码已不足，请及时添加。");
-					notice.setCreateTime(new Date());
-					notice.setNoticeType("UPC/EAN");
-					noticeService.insert(notice);
-				}
-			}
-			SysDeptEntity dept = deptService.selectById(getDeptId());
-			//未发货订单
-			int unshippedNumber = orderService.selectCount(
-					new EntityWrapper<OrderEntity>().in("order_status", ConstantDictionary.OrderStateCode.UNLIQUIDATED_ORDER_STATE)
-							.ne("abnormal_status",ConstantDictionary.OrderStateCode.ORDER_STATE_RETURN)
-			);
-			dept.setUnshippedNumber(unshippedNumber);
-			//未结算订单数(国际已发货)
-			int unliquidatedNumber = orderService.selectCount(
-					new EntityWrapper<OrderEntity>().eq("order_status",ConstantDictionary.OrderStateCode.ORDER_STATE_INTLSHIPPED)
-			);
-			dept.setUnliquidatedNumber(unliquidatedNumber);
-			//预计费用
-			BigDecimal estimatedCost = new BigDecimal(unshippedNumber * 50);
-			dept.setEstimatedCost(estimatedCost);
-			//可用余额
-			BigDecimal availableBalance = dept.getBalance().subtract(estimatedCost).setScale(2,BigDecimal.ROUND_HALF_UP);
-			if(availableBalance.compareTo(new BigDecimal(50)) == -1){
-				SysRoleEntity roleEntity = roleService.selectOne(new EntityWrapper<SysRoleEntity>().eq("role_name","加盟商管理员"));
-				if(sysUserRoleService.selectCount(new EntityWrapper<SysUserRoleEntity>().eq("user_id",getUserId()).eq("role_id",roleEntity.getRoleId())) > 0){
-					NoticeEntity notice = new NoticeEntity();
-					notice.setDeptId(getDeptId());
-					notice.setUserId(getUserId());
-					notice.setNoticeContent("公司可用余额不足，为避免订单出现异常，请及时充值。");
-					notice.setCreateTime(new Date());
-					notice.setNoticeType("余额");
-					noticeService.insert(notice);
-				}
-			}
-			dept.setAvailableBalance(availableBalance);
-			//预计还可生成单数
-			int estimatedOrder = availableBalance.divide(new BigDecimal(50),0,BigDecimal.ROUND_HALF_DOWN).intValue();
-			dept.setEstimatedOrder(estimatedOrder);
-			deptService.updateById(dept);
-		}
-	}
-	
-	/**
-	 * 退出
-	 */
-	@RequestMapping(value = "logout", method = RequestMethod.GET)
-	public String logout() {
-		ShiroUtils.logout();
-		return "redirect:login.html";
-	}
-	
+        new CalculationThread().start();
+        return R.ok();
+
+        // 管理员和员工分离登录时使用
+    /*
+
+        Long id = new Long(0);
+        try {
+            id = sysUserService.selectOne(new EntityWrapper<SysUserEntity>().eq("username", username)).getUserId();
+        }catch (NullPointerException e){
+            return R.error("账号或密码不正确");
+        }
+        // id为1和3的用户不能登录
+        if (id != 1L && id != 3L) {
+            try {
+                Subject subject = ShiroUtils.getSubject();
+                UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+                subject.login(token);
+            } catch (UnknownAccountException e) {
+                return R.error(e.getMessage());
+            } catch (IncorrectCredentialsException e) {
+                return R.error("账号或密码不正确");
+            } catch (LockedAccountException e) {
+                return R.error("账号已被锁定,请联系管理员");
+            } catch (AuthenticationException e) {
+                return R.error("账户验证失败");
+            }
+
+            new CalculationThread().start();
+            return R.ok();
+        } else {
+            return R.error("没有此账号");
+        }
+*/
+
+    }
+
+    class CalculationThread extends Thread {
+        @Override
+        public void run() {
+
+            //总部查询MEN码
+            if (getUserId() == 1L) {
+                int count = eanUpcService.selectCount(new EntityWrapper<EanUpcEntity>().eq("state", 0));
+                if (count < 1000) {
+                    NoticeEntity notice = new NoticeEntity();
+                    notice.setDeptId(getDeptId());
+                    notice.setUserId(getUserId());
+                    notice.setNoticeContent("UPC/EAN码已不足，请及时添加。");
+                    notice.setCreateTime(new Date());
+                    notice.setNoticeType("UPC/EAN");
+                    noticeService.insert(notice);
+                }
+            }
+            SysDeptEntity dept = deptService.selectById(getDeptId());
+            //未发货订单
+            int unshippedNumber = orderService.selectCount(
+                    new EntityWrapper<OrderEntity>().in("order_status", ConstantDictionary.OrderStateCode.UNLIQUIDATED_ORDER_STATE)
+                            .ne("abnormal_status", ConstantDictionary.OrderStateCode.ORDER_STATE_RETURN)
+            );
+            dept.setUnshippedNumber(unshippedNumber);
+            //未结算订单数(国际已发货)
+            int unliquidatedNumber = orderService.selectCount(
+                    new EntityWrapper<OrderEntity>().eq("order_status", ConstantDictionary.OrderStateCode.ORDER_STATE_INTLSHIPPED)
+            );
+            dept.setUnliquidatedNumber(unliquidatedNumber);
+            //预计费用
+            BigDecimal estimatedCost = new BigDecimal(unshippedNumber * 50);
+            dept.setEstimatedCost(estimatedCost);
+            //可用余额
+            BigDecimal availableBalance = dept.getBalance().subtract(estimatedCost).setScale(2, BigDecimal.ROUND_HALF_UP);
+            if (availableBalance.compareTo(new BigDecimal(50)) == -1) {
+                SysRoleEntity roleEntity = roleService.selectOne(new EntityWrapper<SysRoleEntity>().eq("role_name", "加盟商管理员"));
+                if (sysUserRoleService.selectCount(new EntityWrapper<SysUserRoleEntity>().eq("user_id", getUserId()).eq("role_id", roleEntity.getRoleId())) > 0) {
+                    NoticeEntity notice = new NoticeEntity();
+                    notice.setDeptId(getDeptId());
+                    notice.setUserId(getUserId());
+                    notice.setNoticeContent("公司可用余额不足，为避免订单出现异常，请及时充值。");
+                    notice.setCreateTime(new Date());
+                    notice.setNoticeType("余额");
+                    noticeService.insert(notice);
+                }
+            }
+            dept.setAvailableBalance(availableBalance);
+            //预计还可生成单数
+            int estimatedOrder = availableBalance.divide(new BigDecimal(50), 0, BigDecimal.ROUND_HALF_DOWN).intValue();
+            dept.setEstimatedOrder(estimatedOrder);
+            deptService.updateById(dept);
+        }
+    }
+
+    /**
+     * 退出
+     */
+    @RequestMapping(value = "logout", method = RequestMethod.GET)
+    public String logout() {
+        ShiroUtils.logout();
+        return "redirect:login.html";
+    }
+
 }
